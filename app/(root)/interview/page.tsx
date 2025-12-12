311ee7c122b982d33110d711f477bad1fa792920
darshandhara007 <dharaglkard29@gmail.com>
2025-12-12 19:50:16 +0530

    Updated some features

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Agent from "@/components/Agent";
+// import React from 'react'
+// import Agent from "@/components/Agent";
 
-const Page = () => {
+// const Page = () => {
+//     return (
+//         <>
+//             <h3>Interview Generation</h3>
+
+//             <Agent userName="You" userId="user1" type="generate" />
+//         </>
+//     )
+// }
+// export default Page
+
+import Agent from "@/components/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
+
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
         <>
             <h3>Interview Generation</h3>
 
-            <Agent userName="You" userId="user1" type="generate" />
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
         </>
     )
 }
